4b46c5d7160da35e6911f45f19bb38c7a9e577af
lavanya33 <lavanya.matta@alphanove.co.uk>
2023-04-12 15:06:32 +0100

    added graphQL code in app.js

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "authflowdemo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "authflowdemo": {
       "AppClientID": "string",
